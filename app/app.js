72a0e4550b35b24d5d143c23c1b3da8c241efe15
tantch <ei12019@fe.up.pt>
2015-11-25 11:24:00 +0000

    cart adicionado

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@
 
 var bookApp = angular.module('bookApp', [
   'ngRoute',
-  'bookControllers'
+  'bookControllers',
+  'door3.css'
 ]);
 
 bookApp.config(['$routeProvider',
@@ -10,11 +11,18 @@ bookApp.config(['$routeProvider',
     $routeProvider.
       when('/home', {
         templateUrl: 'partials/home.html',
-        controller: 'HomeCtrl'
+        controller: 'HomeCtrl',
+         css: 'css/shop-homepage.css'
       }).
       when('/book/:bookId', {
         templateUrl: 'partials/book.html',
-        controller: 'BookCtrl'
+        controller: 'BookCtrl',
+        css: 'css/shop-item.css'
+      }).
+      when('/cart',{
+        templateUrl: 'partials/cart.html',
+        controller: 'CartCtrl',
+        css: 'css/shop-homepage.css'
       }).
       otherwise({
         redirectTo: '/home'
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,40 @@ var bookControllers = angular.module('bookControllers', [
 
 bookControllers.controller('HomeCtrl', function ($scope,$http) {
 
+  $(function() {
+    var pop = $('.popbtn');
+    var row = $('.row:not(:first):not(:last)');
+
+
+    pop.popover({
+      trigger: 'manual',
+      html: true,
+      container: 'body',
+      placement: 'bottom',
+      animation: false,
+      content: function() {
+        return $('#popover').html();
+      }
+    });
+
+
+    pop.on('click', function(e) {
+      pop.popover('toggle');
+      pop.not(this).popover('hide');
+    });
+
+    $(window).on('resize', function() {
+      pop.popover('hide');
+    });
+
+    row.on('touchend', function(e) {
+      $(this).find('.popbtn').popover('toggle');
+      row.not(this).find('.popbtn').popover('hide');
+      return false;
+    });
+
+  });
+
   $http({
     url: "http://127.0.0.1:49822/api/artigos/listaRating/12",
     method: "GET",
@@ -25,5 +59,12 @@ bookControllers.controller('BookCtrl', function ($scope,$http,$routeParams) {
     method: "GET",
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   }).success(function(response) {$scope.book = response;});
+
+});
+
+bookControllers.controller('CartCtrl',function ($scope,$http){
+
+
+
 
 });
